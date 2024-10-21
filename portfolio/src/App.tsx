0a3ad9f81de3b6446988c203bc16b51514d0aa87
hsw5710 <pett1475321@gmail.com>
2024-10-21 22:38:52 +0900

    test

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <h2>페이지 만들기 시작</h2>
+      <a href="https://www.notion.so/0cc8a3f5ec974cd5bcf685f76e35a6b7">
+        코리아 it 4~9월 공부 내용 정리 노션
+      </a>
     </div>
   );
 }
